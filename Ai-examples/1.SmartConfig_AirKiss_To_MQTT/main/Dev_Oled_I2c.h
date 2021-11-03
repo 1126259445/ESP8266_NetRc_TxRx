0abdca7e00b5c29c3595e29c3ff871dd4b926a60
1126259445 <1126259445@qq.com>
2021-11-03 08:37:51 -0700

    add 16*32 Ascall

--- Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/Dev_Oled_I2c.h
+++ Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/Dev_Oled_I2c.h
@@ -9,7 +9,9 @@
 #define		OLED_CMD  	0		// 命令
 #define		OLED_DATA 	1		// 数据
 
-#define 	SIZE 		16		//显示字符的大小
+#define 	SIZE16 		16		//显示字符的大小8*16
+#define     SIZE32      32      //显示字符的大小16*32
+
 #define 	Max_Column	128		//最大列数
 #define		Max_Row		64		//最大行数
 #define		X_WIDTH 	128		//X轴的宽度
@@ -24,7 +26,7 @@ void  OLED_I2C_Init(void);
 void  OLED_Clear(void);
 void  OLED_DrawBMP(uint8_t x0, uint8_t y0,uint8_t x1, uint8_t y1,uint8_t BMP[]);
 void  OLED_ShowIP(uint8_t x, uint8_t y, char*Array_IP);
-void  OLED_ShowString(uint8_t x, uint8_t y, char * Show_char);
+void  OLED_ShowString(uint8_t x, uint8_t y, char * Show_char,uint8_t fount_size);
 
 #endif
 
