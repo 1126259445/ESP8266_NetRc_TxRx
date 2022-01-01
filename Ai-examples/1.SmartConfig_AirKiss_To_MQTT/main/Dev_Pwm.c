9e1efbc9f2be10dc24e65953f880c5ababe8c768
1126259445 <1126259445@qq.com>
2021-12-31 22:39:31 -0800

    修改PWM通道IO

--- Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/Dev_Pwm.c
+++ Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/Dev_Pwm.c
@@ -26,13 +26,13 @@
 
 #define PWM_ALL_CH_NUM      8
 
-#define PWM_1_OUT_IO_NUM   2
-#define PWM_2_OUT_IO_NUM   4
-#define PWM_3_OUT_IO_NUM   5
+#define PWM_1_OUT_IO_NUM   15
+#define PWM_2_OUT_IO_NUM   13
+#define PWM_3_OUT_IO_NUM   2
 #define PWM_4_OUT_IO_NUM   12
-#define PWM_5_OUT_IO_NUM   13
-#define PWM_6_OUT_IO_NUM   14
-#define PWM_7_OUT_IO_NUM   15
+#define PWM_5_OUT_IO_NUM   4
+#define PWM_6_OUT_IO_NUM   5
+#define PWM_7_OUT_IO_NUM   14
 #define PWM_8_OUT_IO_NUM   16
 
 // PWM period 20000us(50hz), same as sover
